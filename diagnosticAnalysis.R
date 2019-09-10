e5f1121568b60a2dd9fcb0be7e518ba3352e6612
gpadolina <47703315+gpadolina@users.noreply.github.com>
2019-09-09 22:49:23 -0700

    Update diagnosticAnalysis.R

--- diagnosticAnalysis.R
+++ diagnosticAnalysis.R
@@ -5,3 +5,21 @@ library(tidyr)
 library(VIF)
 
 remove(list = ls())
+
+# Data exploration
+View(swiss)
+
+# Summary provides statistics of the dataset
+summary(swiss)
+
+# Dataset correlation
+cor(swiss)
+
+# Identifying the presence of multicollinearity
+swiss <- swiss
+
+model <- lm(Fertility ~ Agriculture + Examination + Education + Catholic +
+            Infant.Mortality, data = swiss)
+
+# First model
+summary(model)
