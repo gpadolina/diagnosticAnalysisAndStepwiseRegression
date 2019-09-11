fe47e1a872eac3f8b88f79c4ba732e8f640dc83c
gpadolina <47703315+gpadolina@users.noreply.github.com>
2019-09-10 21:45:55 -0700

    Update diagnosticAnalysis.R

--- diagnosticAnalysis.R
+++ diagnosticAnalysis.R
@@ -14,6 +14,7 @@ summary(swiss)
 
 # Dataset correlation
 cor(swiss)
+ggpairs(swiss)
 
 # Identifying the presence of multicollinearity
 swiss <- swiss
@@ -23,3 +24,31 @@ model <- lm(Fertility ~ Agriculture + Examination + Education + Catholic +
 
 # First model
 summary(model)
+
+# Normality plot
+qqnorm(residuals(model), ylab = "Residuals", main = " ")
+qqline(residuals(model))
+
+# Normality test
+shapiro.test(residuals(model))
+
+# Plot to test constant variance of errors
+plot(fitted(model), residuals(model), xlab = "Fitted", ylab = "Residuals")
+abline(h = 0)
+
+# Constant variance of errors
+plot(swiss$Agriculture, residuals(model), xlab = "Examination", ylab = "Residuals")
+plot(swiss$Examination, residuals(model), xlab = "Examination", ylab = "Residuals")
+plot(swiss$Education, residuals(model), xlab = "Examination", ylab = "Residuals")
+plot(swiss$Catholic, residuals(model), xlab = "Examination", ylab = "Residuals")
+plot(swiss$Infant.Mortality, residuals(model), xlab = "Examination", ylab = "Residuals")
+
+# F-test of equal variance of errors among two groups of Catholic
+var.test(residuals(model)[swiss$Catholic > 60], residuals(model)[swiss$Catholic < 60])
+
+n <- length(residuals(model))
+
+plot(tail(residuals(model), n-1) ~ head(residuals(model), n-1),
+     xlab = expression(hat(epsilon)[i]), ylab = expression(hat(epsilon)[i + 1]))
+
+# Plot to test independent errors
